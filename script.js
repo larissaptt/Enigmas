eab533b8f0724edc781a7b93de84a330bd910a7e
Larissa <pottkerlarissa@gmail.com>
2026-04-19 16:43:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
- const checkbox = document.getElementById("aceitar");
-  const botao = document.getElementById("botao_iniciar");
+const checkbox = document.getElementById("aceitar");
+const botao = document.getElementById("botao_iniciar");
 
   checkbox.addEventListener("change", function() {
     botao.disabled = !checkbox.checked;
-  });
+});
+document.getElementById("botao_iniciar").addEventListener("click", () => {
+    window.location.href = "fases/2003.html";
+});
